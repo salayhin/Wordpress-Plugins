3e8505fa9e89058b5d66655bc8596402b38e1f24
Sirajus Salayhin <salayhin@gmail.com>
2012-01-25 04:07:02 +0600

    Modified File

--- linkedin-share/linkedin_share.php
+++ linkedin-share/linkedin_share.php
@@ -23,26 +23,27 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-function linkedin_share($content) {
+function linkedin_share($content)
+{
 
-	if ( is_single() ) {
+    if (is_single()) {
 
-		$protocol = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
+        $protocol = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
 
-		$sc_page_url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        $sc_page_url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-	    $post_id = get_the_ID();
+        $post_id = get_the_ID();
 
-		$sc_page_title = get_the_title($post_id);
+        $sc_page_title = get_the_title($post_id);
 
-$content .= '<p><script src="http://platform.linkedin.com/in.js" type="text/javascript"></script>
+        $content .= '<p><script src="http://platform.linkedin.com/in.js" type="text/javascript"></script>
 <script type="IN/Share" data-counter="right"></script></p>';
 
-		}
+    }
 
-	return $content;
+    return $content;
 }
 
-add_action('the_content','linkedin_share');
+add_action('the_content', 'linkedin_share');
 
 ?>
